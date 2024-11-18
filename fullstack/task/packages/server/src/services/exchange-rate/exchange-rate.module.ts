d4b2666997d7418a285adb406b80797d0111641b
Michal <michal.olsav@gmail.com>
2024-11-18 08:44:58 +0100

    fix: remove bull module refs

--- fullstack/task/packages/server/src/services/exchange-rate/exchange-rate.module.ts
+++ fullstack/task/packages/server/src/services/exchange-rate/exchange-rate.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BullModule } from '@nestjs/bull';
 import { ExchangeRateService } from './exchange-rate.service';
 import { ExchangeRateResolver } from './exchange-rate.resolver';
 import { ExchangeRate } from './entities';
 
 @Module({
-    imports: [
-        TypeOrmModule.forFeature([ExchangeRate]),
-        BullModule.registerQueueAsync({
-            name: 'exchangeRate',
-        }),
-    ],
+    imports: [TypeOrmModule.forFeature([ExchangeRate])],
     providers: [ExchangeRateService, ExchangeRateResolver],
     exports: [ExchangeRateService],
 })
