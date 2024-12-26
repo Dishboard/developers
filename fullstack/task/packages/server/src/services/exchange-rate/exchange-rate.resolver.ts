24b7f99d93dc566ca835a3dcc9df761d73d6e787
Bekhzod96 <mr.begzodjon96@mail.ru>
2024-12-26 12:53:24 +0500

    feat: fetching exchange rates

--- fullstack/task/packages/server/src/services/exchange-rate/exchange-rate.resolver.ts
+++ fullstack/task/packages/server/src/services/exchange-rate/exchange-rate.resolver.ts
@@ -5,9 +5,9 @@ import { ExchangeRateService } from './exchange-rate.service';
 export class ExchangeRateResolver {
     constructor(private readonly exchangeRateService: ExchangeRateService) {}
 
-    // TODO: Implement a GraphQL Query that returns the exchange rates
     @Query(() => String)
     async exchangeRates(): Promise<string> {
-        return 'Hello';
+        return this.exchangeRateService.getExchangeRates();
+
     }
 }
--- fullstack/task/packages/server/src/services/exchange-rate/exchange-rate.service.ts
+++ fullstack/task/packages/server/src/services/exchange-rate/exchange-rate.service.ts
@@ -1,11 +1,39 @@
 import { Injectable } from '@nestjs/common';
-
+import axios from 'axios';
 @Injectable()
 export class ExchangeRateService {
-    public getExchangeRates = async () => {
-        // TODO: Implement the fetching and parsing of the exchange rates.
-        // Use this method in the resolver.
+    public getExchangeRates = async (): Promise<string>  => {
+        
+        // Found endpoints -
+        // XML - https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.xml
+        // TXT -https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt
 
-        return [];
+        const url = 'https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt';
+        
+        try {
+            const response = await axios.get(url);
+            const data = response.data;
+            
+            const lines = data.split('\n');
+            const exchangeRates = [];
+            
+            for (let i = 2; i < lines.length; i++) {
+                const line = lines[i];
+                if (line.trim() === '') continue;
+                
+                const [country, currency, amount, code, rate] = line.split('|');
+                exchangeRates.push({
+                    country: country.trim(),
+                    currency: currency.trim(),
+                    amount: parseInt(amount.trim(), 10),
+                    code: code.trim(),
+                    rate: parseFloat(rate.trim())
+                });
+            }
+            return JSON.stringify(exchangeRates);
+        } catch (error) {
+            console.error('Error fetching exchange rates:', error);
+            return "Oops, something went wrong!";
+        }
     };
 }
