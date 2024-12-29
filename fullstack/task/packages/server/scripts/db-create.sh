3c4d26304893f77e2ca0223f32ecd51ec438ccbd
Pashazade <nazar.pashazade@accenture.com>
2024-12-30 01:40:14 +0200

    Feat: add scripts to run pgadmin container after DB  creation

--- fullstack/task/packages/server/scripts/db-create.sh
+++ fullstack/task/packages/server/scripts/db-create.sh
@@ -1,17 +1,40 @@
-echo "Removing old docker container..."
-(docker stop dishboard-dev-task-db && docker kill dishboard-dev-task-db || :) && (docker rm dishboard-dev-task-db || :)
+#!/bin/bash
 
-echo "Creating a new instance..."
+# 1. Remove old containers if they exist
+echo "Removing old docker containers..."
+(docker stop dishboard-dev-task-db pgadmin || :) && (docker rm dishboard-dev-task-db pgadmin || :)
+
+# 2. Create Docker network if it doesn't exist
+echo "Creating Docker network..."
+docker network create pg-network || echo "Network pg-network already exists."
+
+# 3. Start PostgreSQL container
+echo "Starting PostgreSQL container..."
 docker run \
     --name dishboard-dev-task-db \
     -e POSTGRES_PASSWORD=postgres \
     -e PGPASSWORD=postgres \
     -p 5430:5432 \
+    --network pg-network \
     -d postgres
 
-echo "Waiting for the database to start..."
-sleep 3
+# 4. Start pgAdmin container
+echo "Starting pgAdmin container..."
+docker run \
+    --name pgadmin \
+    -e PGADMIN_DEFAULT_EMAIL=pgadminuser@gmail.com \
+    -e PGADMIN_DEFAULT_PASSWORD=Database123! \
+    -p 8080:80 \
+    --network pg-network \
+    -d dpage/pgadmin4:latest
 
+# 5. Wait for the database to start
+echo "Waiting for the database to start..."
+sleep 5
+
+# 6. Create the database
 echo "Creating the database..."
-echo "CREATE DATABASE dev;" | docker exec -i dishboard-dev-task-db psql -U postgres
-echo "\l" | docker exec -i dishboard-dev-task-db psql -U postgres
+docker exec -i dishboard-dev-task-db psql -U postgres -c "CREATE DATABASE dev;"
+
+# 7. Provide instructions
+echo "PostgreSQL and pgAdmin setup complete. pgAdmin is available at http://localhost:8080/."
