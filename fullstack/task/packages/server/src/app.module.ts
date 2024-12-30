a5b012c1f03c431cc41fae8a2d517f3ba253a154
Igor Bachev <igor.bachev95@gmail.com>
2024-12-30 18:39:17 +0100

    Backend configs

--- fullstack/task/packages/server/src/app.module.ts
+++ fullstack/task/packages/server/src/app.module.ts
@@ -5,11 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { graphqlConfig, typeormConfig } from './config';
 import { modules } from './entity-modules';
 import { ExchangeRateModule } from './services/exchange-rate/exchange-rate.module';
+import { exchangeRateConfig } from './config/exchange-rates';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
+            load: [() => ({ exchangeRate: exchangeRateConfig })], 
         }),
         TypeOrmModule.forRoot(typeormConfig),
         GraphQLModule.forRoot(graphqlConfig),
